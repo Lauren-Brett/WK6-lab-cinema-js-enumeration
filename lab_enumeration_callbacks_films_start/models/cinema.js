fdef1b0c8ad25184465d7ee91d63beac622b861e
lauren brett <laurencareybrett@gmail.com>
2020-01-08 14:09:50 +0000

    6 pass tests

--- lab_enumeration_callbacks_films_start/models/cinema.js
+++ lab_enumeration_callbacks_films_start/models/cinema.js
@@ -2,4 +2,11 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+
+Cinema.prototype.titlesOfFilms = function (films) {
+    const filmTitles = films.map((film) => {
+      return film.title;
+    })
+    return filmTitles
+};
 module.exports = Cinema;
